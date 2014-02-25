e1b156e4a4ccca7b240b125ca7368250fc722366
Andrew Hart <Andrew@ProjectDent.com>
2014-02-25 09:12:21 +0000

    adds infinite paging scroll view

---
+++ DMLazyScrollViewExample/DMLazyScrollView/PDInfinitePagingScrollView.h
@@ -0,0 +1,13 @@
+//
+//  PDInfinitePagingScrollView.h
+//  DMLazyScrollViewExample
+//
+//  Created by Andrew Hart on 25/02/2014.
+//  Copyright (c) 2014 daniele. All rights reserved.
+//
+
+#import "PDPagingScrollView.h"
+
+@interface PDInfinitePagingScrollView : PDPagingScrollView
+
+@end
